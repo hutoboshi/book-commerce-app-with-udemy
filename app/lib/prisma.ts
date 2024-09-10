29dd41e833bda6045c57ab7eb7d798eb7ab535cc
taisei <hutoboshi.eng@gmail.com>
2024-09-10 23:59:33 +0900

    認証機能

---
+++ app/lib/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+const glovalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+if (!glovalForPrisma.prisma){
+  glovalForPrisma.prisma = new PrismaClient()
+}
+
+prisma = glovalForPrisma;
+
+export default prisma
